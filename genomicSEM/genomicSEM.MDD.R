830968b427842f5835fc59499de4ab5740bb33f9
Na Cai <caina89@gmail.com>
2023-02-15 02:50:49 +0100

    Update genomicSEM.MDD.R

--- genomicSEM/genomicSEM.MDD.R
+++ genomicSEM/genomicSEM.MDD.R
@@ -5,59 +5,50 @@ require(stats)
 library(psych)
 library(GPArotation)
 
-## lifetime symptoms + lifetimeMDD 
-setwd("/no-backup/caina/biobank/genomicSEM/mungestats")
-traits=c(paste0("Munge_LifetimeSymptoms_A",c(1:4,6:9),".sumstats.gz"),"munge_ukbmdd.sumstats.gz")
+## LD reference
+ld="../ldsc/"
+wld="../ldsc/"
+
+## prevalence 
+prev=read.table("prevall.txt",header=T)
+
+## FOR iPSYCH2012 and iPSYCH2015i MDD, change "LifetimeMDD" to "iPSYCH2012" or "iPSYCH2015i"
+
+## WorstEpisode symptoms + LifetimeMDD 
+traits=c(paste0("WorstEpisode_A",c(1:4,6:9),".sumstats.gz"),"LifetimeMDD.sumstats.gz")
 trait.names=c(paste0("A",c(1:4,6:9)),"LifetimeMDD")
-prev=read.table("Prevalence_Phenos_FullSample.txt",header=T)
 sample.prev=c(prev$Prevalence[match(trait.names,prev$Pheno)],0.243)
 population.prev=sample.prev
-ld="/projects/biobank/release2/ldscores/maf5/"
-wld="/projects/biobank/release2/ldscores/maf5/"
-
 LDSCoutput=ldsc(traits, sample.prev, population.prev, ld, wld, trait.names)
 CommonFactor_DWLS=commonfactor(covstruc = LDSCoutput, estimation="DWLS")
 k<-nrow(LDSCoutput$S)
 SE<-matrix(0, k, k)
 SE[lower.tri(SE,diag=TRUE)] <-sqrt(diag(LDSCoutput$V))
-
 save(LDSCoutput, file="WE.LifetimeMDD.genomicSEM.RData")
 
-## PHQ9 symptoms + lifetimeMDD 
-setwd("/no-backup/caina/biobank/genomicSEM/mungestats")
-traits=c(paste0("Munge_LenCurrent_A",c(1:9),".sumstats.gz"),"munge_ukbmdd.sumstats.gz")
+## PHQ9 symptoms + LifetimeMDD 
+traits=c(paste0("PHQ9_A",c(1:9),".sumstats.gz"),"LifetimeMDD.sumstats.gz")
 trait.names=c(paste0("A",c(1:9)),"LifetimeMDD")
-prev=read.table("PrevalenceTable_LenCurrent.txt",header=T)
 prev$Pheno=gsub("lencurrent","",prev$Pheno)
 sample.prev=c(prev$Prev[match(paste0("A",c(1:9)),prev$Pheno)],0.243)
 population.prev=sample.prev
-ld="/projects/biobank/release2/ldscores/maf5/"
-wld="/projects/biobank/release2/ldscores/maf5/"
-
 LDSCoutput=ldsc(traits, sample.prev, population.prev, ld, wld, trait.names)
 CommonFactor_DWLS=commonfactor(covstruc = LDSCoutput, estimation="DWLS")
 k<-nrow(LDSCoutput$S)
 SE<-matrix(0, k, k)
 SE[lower.tri(SE,diag=TRUE)] <-sqrt(diag(LDSCoutput$V))
-
 save(LDSCoutput, file="PHQ9.LifetimeMDD.genomicSEM.RData")
 
 ## PHQ9Skip + PHQA1A2 symptoms + lifetimeMDD 
-setwd("/no-backup/caina/biobank/genomicSEM/mungestats")
-traits=c(paste0("Munge_LenCurrent_A",c(1:2),".sumstats.gz"),paste0("Munge_LenStop_A",c(3:9),".sumstats.gz"),"munge_ukbmdd.sumstats.gz")
+traits=c(paste0("WorstEpisode_A",c(1:2),".sumstats.gz"),paste0("PHQ9Skip_A",c(3:9),".sumstats.gz"),"LifetimeMDD.sumstats.gz")
 trait.names=c(paste0("A",c(1:2)),paste0("SkipA",c(3:9)),"LifetimeMDD")
-prev=read.table("prevall.txt",header=T)
 prev1=prev[which(prev$data=="phq"),]
 prev2=prev[which(prev$data=="phqskip"),]
 sample.prev=c(prev1$samp_prev[match(paste0("A",c(1:2)),prev1$symp)],prev2$samp_prev[match(paste0("A",c(3:9)),prev2$symp)],0.243)
 population.prev=sample.prev
-ld="/projects/biobank/release2/ldscores/maf5/"
-wld="/projects/biobank/release2/ldscores/maf5/"
-
 LDSCoutput=ldsc(traits, sample.prev, population.prev, ld, wld, trait.names)
 CommonFactor_DWLS=commonfactor(covstruc = LDSCoutput, estimation="DWLS")
 k<-nrow(LDSCoutput$S)
 SE<-matrix(0, k, k)
 SE[lower.tri(SE,diag=TRUE)] <-sqrt(diag(LDSCoutput$V))
-
 save(LDSCoutput, file="PHQ9SkipA1A2.LifetimeMDD.genomicSEM.RData")
